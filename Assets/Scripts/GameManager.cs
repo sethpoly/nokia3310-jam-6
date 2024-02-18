45fd5064a9b7521d98fc6202db727802e4c30539
Seth <seth@seths-mbp.mynetworksettings.com>
2024-02-18 18:13:08 -0500

    [level-prefab] - add level prefab + level switching logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,23 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager: MonoBehaviour
 {
     [SerializeField] private TransitionHandler transitionHandler;
+    [SerializeField] private List<Level> levels = new();
+    [SerializeField] private int currentLevelIndex;
+
+    void Awake()
+    {
+        if(levels.Count > 0)
+        {
+             currentLevelIndex = 0;
+             RestartLevel();
+        } else
+        {
+            Debug.LogError("Cannot start first level. No levels in list");
+        }
+    }
 
     void Update()
     {
@@ -11,5 +26,45 @@ public class GameManager: MonoBehaviour
         {
             StartCoroutine(transitionHandler.LoadLevel(1));
         }
+    }
+
+    public void NextLevel()
+    {
+        DisposeLevel(levels[currentLevelIndex]);
+        int nextIndex = currentLevelIndex + 1;
+
+        if(levels.Count -1 >= nextIndex)
+        {
+            StartLevel(levels[nextIndex]);
+            currentLevelIndex = nextIndex;
+        } else
+        {
+            Debug.LogError("No next level found");
+        }
+    }
+
+    public void RestartLevel()
+    {
+        if (levels.Count - 1 >= currentLevelIndex) 
+        {
+            Debug.Log("Restarting current level: " + levels[currentLevelIndex].title);
+            DisposeLevel(levels[currentLevelIndex]);
+            StartLevel(levels[currentLevelIndex]);
+        } else
+        {
+            Debug.LogError("Cannot restart level. No level found");
+        }
+    }
+
+    private void StartLevel(Level level)
+    {
+        Debug.Log("Starting level " + level.title);
+        Instantiate(level);
+    }
+
+    private void DisposeLevel(Level level)
+    {
+        Debug.Log("Disposing level " + level.title);
+        Destroy(level.gameObject);
     }
 }
