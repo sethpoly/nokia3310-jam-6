be54f2d80a0528aab2f7fe3055463803c05d96eb
Seth <seth@seths-mbp.mynetworksettings.com>
2024-02-19 09:31:34 -0500

    [basic-levels] - reset player balloons on next level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -50,6 +50,9 @@ public class GameManager: MonoBehaviour
         {
             StartLevel(nextIndex, () => {
                 DisposeLevel(currentLevelIndex);
+
+                // Reset player incase balloons were popped
+                RespawnPlayer();
             });
         } else
         {
