4a8cf5ac4759c6b1ccc4daea5599803a60f0e2c8
Seth <seth@seths-mbp.mynetworksettings.com>
2024-02-18 19:13:13 -0500

    [level-prefab] - fix transition anim

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,23 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager: MonoBehaviour
 {
     [SerializeField] private TransitionHandler transitionHandler;
     [SerializeField] private List<Level> levels = new();
-    [SerializeField] private int currentLevelIndex;
+    [SerializeField] private int currentLevelIndex = -1;
+
+    [SerializeField] private GameObject player;
+
+    [SerializeField] private Vector2 playerStartingPosition;
 
     void Awake()
     {
+        playerStartingPosition = player.transform.position;
         if(levels.Count > 0)
         {
-             currentLevelIndex = 0;
-             RestartLevel();
+             NextLevel();
         } else
         {
             Debug.LogError("Cannot start first level. No levels in list");
@@ -22,21 +27,20 @@ public class GameManager: MonoBehaviour
     void Update()
     {
         // Testing
-        if(Input.GetKeyDown(KeyCode.T))
+        if(Input.GetKeyDown(KeyCode.N))
         {
-            StartCoroutine(transitionHandler.LoadLevel(1));
+            NextLevel();
         }
     }
 
     public void NextLevel()
     {
-        DisposeLevel(levels[currentLevelIndex]);
+        DisposeLevel(currentLevelIndex);
         int nextIndex = currentLevelIndex + 1;
 
-        if(levels.Count -1 >= nextIndex)
+        if(levels.Count - 1 >= nextIndex)
         {
-            StartLevel(levels[nextIndex]);
-            currentLevelIndex = nextIndex;
+            StartLevel(nextIndex);
         } else
         {
             Debug.LogError("No next level found");
@@ -48,23 +52,38 @@ public class GameManager: MonoBehaviour
         if (levels.Count - 1 >= currentLevelIndex) 
         {
             Debug.Log("Restarting current level: " + levels[currentLevelIndex].title);
-            DisposeLevel(levels[currentLevelIndex]);
-            StartLevel(levels[currentLevelIndex]);
+            DisposeLevel(currentLevelIndex);
+            StartLevel(currentLevelIndex);
         } else
         {
             Debug.LogError("Cannot restart level. No level found");
         }
     }
 
-    private void StartLevel(Level level)
+    private void StartLevel(int levelIndex)
     {
-        Debug.Log("Starting level " + level.title);
-        Instantiate(level);
+        if(levels.Count - 1 < levelIndex) 
+        {
+            Debug.LogError("Cannot start level with index " + levelIndex);
+            return;
+        }
+        Level level = levels[levelIndex];
+        StartCoroutine(transitionHandler.LoadLevel(1, onCompletion: () => {
+            Instantiate(level);
+            player.transform.position = playerStartingPosition;
+            currentLevelIndex = levelIndex;
+            Debug.Log("Starting level " + level.title);
+        }));
     }
 
-    private void DisposeLevel(Level level)
+    private void DisposeLevel(int levelIndex)
     {
-        Debug.Log("Disposing level " + level.title);
+        if(levels.ElementAtOrDefault(levelIndex) == null) 
+        {
+            Debug.LogError("Cannot dispose level with index " + levelIndex);
+            return;
+        }
+        Level level = levels[levelIndex];
         Destroy(level.gameObject);
     }
 }
