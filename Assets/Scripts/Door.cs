e2b49662c109bd46215e2844160f1379cc36eecd
Seth <seth@seths-mbp.mynetworksettings.com>
2024-02-19 20:13:15 -0500

    [await-coin-for-door] - refactor door to open on all coins collected

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,10 +2,24 @@ using UnityEngine;
 
 public class Door: MonoBehaviour
 {
-    [SerializeField] private Animator animator;
+    private Animator animator;
+    private BoxCollider2D boxCollider;
+    private bool isOpen = false;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        boxCollider.enabled = isOpen;
+    }
 
     public void OpenDoor()
     {
         animator.Play("Door_Open");
+        isOpen = true;
     }
 }
